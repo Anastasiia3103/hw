82acb9e4bbeac9b2eaeb1fbea4b8406e444e55ce
Anastasiia <lavrentjeva97@gmail.com>
2023-03-10 09:21:19 +0100

    Initial commit

---
+++ src/hw6/Car.java
@@ -0,0 +1,23 @@
+package hw6;
+
+public class Car {
+    public void start() {
+
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+        System.out.println("Engine is started");
+    }
+
+    private void startElectricity() {
+        System.out.println("Start electricity");
+    }
+
+    private void startCommand() {
+        System.out.println("Start Command");
+    }
+
+    private void startFuelSystem() {
+        System.out.println("Start fuel system");
+    }
+}
---
+++ src/hw6/Employee.java
@@ -0,0 +1,58 @@
+package hw6;
+
+public class Employee {
+
+    private String fullName;
+    private String position;
+    private String email;
+    private int phoneNumber;
+    private int age;
+
+    public Employee (String fullName, String position, String email, int phoneNumber, int age){
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.fullName = fullName;
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
